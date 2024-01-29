cf6f1a37a990b147935e575c6b052d6e68feff6a
Kyle Ivan Barreras <KyleIvanTBarreras@yahoo.com>
2024-01-29 11:44:51 -0800

    Portfolio version 3

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+function showDescription(container) {
+    container.querySelector('.project-description').style.opacity = 1;
+    container.querySelector('.project-description').style.display = 'block';
+}
+
+function hideDescription(container) {
+    container.querySelector('.project-description').style.opacity = 0;
+    container.querySelector('.project-description').style.display = 'none';
+}
